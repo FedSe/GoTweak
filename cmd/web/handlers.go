5926a9acab6c380dda8ca6595a96a07f7dd80bce
FedSe <68391609+FedSe@users.noreply.github.com>
2024-01-26 13:22:45 +0700

    Add files via upload

---
+++ cmd/web/handlers.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+	"log"
+	"io"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w) // Использование помощника notFound()
+		return
+	}
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+	}
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w) // Использование помощника notFound()
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed) // Используем помощник clientError()
+		return
+	}
+
+	newkey := "ImportHomepage"
+	newbrowser := 5
+
+	id, err := app.policies.Insert(newkey, newbrowser)
+	if err != nil {
+		app.serverError(w, err)
+		return
+
+	}
+ 
+	// Перенаправляем пользователя на соответствующую страницу заметки.
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}	
+
+	w.Write([]byte("Привет, жаным...\n"))
+	w.Write(b)
+}
